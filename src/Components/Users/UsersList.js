96a88509e26f54f3265172e21f850822af6ba312
masoudChakeri <masoud.chakeri@gmail.com>
2023-03-08 19:39:33 +0330

    end

---
+++ src/Components/Users/UsersList.js
@@ -0,0 +1,17 @@
+import Card from "../Ui/Card";
+
+import classes from "./UsersList.module.css";
+
+const UsersList = (props) => {
+    return (
+        <Card className={classes.users}>
+            <ul>
+                {props.users.map(user => (
+                    <li key={user.key}>{user.name} ({user.age} years old)</li>
+                ))}
+            </ul>
+        </Card>
+    )
+}
+
+export default UsersList
